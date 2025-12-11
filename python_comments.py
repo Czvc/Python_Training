b438f92da452e7c9cf328393ec89abf65607459b
Chase <zedrikchase@gmail.com>
2025-12-11 14:29:45 +0800

    Python Comments
    Syntax to create comments that I commonly use to label and describe each step that I've done

---
+++ python_comments.py
@@ -0,0 +1,10 @@
+# Comments starts with a #, and Python will ignore them:
+#This is a comment
+print("Gagraduate ako!")
+
+#Comments can be placed at the end of a line, and Python will ignore the rest of the line:
+print("Gagraduate ako!")  # This is a comment
+
+# A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:
+# print("This will not be executed")
+print("This will be executed")
