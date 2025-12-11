ac42b2e7cbf1b8a164532392abb9a724c3d56b85
Chase <zedrikchase@gmail.com>
2025-12-11 14:30:53 +0800

    Python Variables
    This shows the basics on how to create variables

---
+++ python_variables.py
@@ -0,0 +1,35 @@
+"""Python has no command for declaring a variable.
+A variable is created the moment you first assign a value to it."""
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+x = 4       # x is of type int
+x = "Chase" # x is now of type str
+print(x)
+
+# If you want to specify the data type of a variable, this can be done with casting.
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+# You can get the data type of a variable with the type() function.
+x = 5
+y = "Chase"
+print(type(x))
+print(type(y))
+
+# String variables can be declared either by using single or double quotes:
+x = "Kyrie"
+# is the same as
+x = 'Kyrie'
+
+# Variable names are case-sensitive.
+a = 1
+A = "LeBron"
+# A will not overwrite a
