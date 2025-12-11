cafb37560a05c456bdc3f3555c016f8f172d3546
Chase <zedrikchase@gmail.com>
2025-12-11 17:51:32 +0800

    Concatenate Strings
    Combine strings using concatenate

---
+++ python_concatenate_strings.py
@@ -0,0 +1,10 @@
+# To concatenate, or combine, two strings you can use the + operator.
+string1 = "Hello, "
+string2 = "Ortigas!"
+result = string1 + string2
+print(result)  # Output: Hello, Ortigas!
+
+# You can also use the join() method to concatenate strings in a list.
+strings = ["Hello", " ", "Ortigas", "!"]
+result = "".join(strings)
+print(result)  # Output: Hello Ortigas!
