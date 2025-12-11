36540de4c1564d00c4783d165d4e61a95cee0318
Chase <zedrikchase@gmail.com>
2025-12-11 14:38:24 +0800

    Assign Multiple Values
    Assigning values to multiple variables in one line

---
+++ python_assign_multiple_values.py
@@ -0,0 +1,18 @@
+# Python allows you to assign values to multiple variables in one line:
+x, y, z = "Toyota", "Nissan", "Honda"
+print(x)
+print(y)    
+print(z)
+
+# And you can assign the same value to multiple variables in one line:
+a = b = c = "Orange"
+print(a)
+print(b)
+print(c)
+
+# You can also unpack a collection:
+cars = ["Toyota", "Nissan", "Honda"]
+x, y, z = cars
+print(x)
+print(y)
+print(z)
