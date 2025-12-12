2b71f4139dca985fc7229c4fb5d0e57afd576a38
Chase <zedrikchase@gmail.com>
2025-12-12 12:24:54 +0800

    Logical Operators
    Python logical operators combine conditions and return True or False.

---
+++ python_logical_operators.py
@@ -0,0 +1,21 @@
+# Logical operators are used to combine conditional statements:
+a = 10
+b = 5
+
+# AND operator  
+print("This is AND operator:")
+if a > 5 and b < 10:
+    print("Both conditions are True")
+
+# OR operator
+print("This is OR operator:")
+if a > 5 or b > 10:
+    print("At least one condition is True")
+
+# NOT operator
+print("This is NOT operator:")
+if not a < 5:
+    print("The condition is False, so NOT makes it True")
+
+
+
