7e9ff73b897f524df11144ca8631aeebd8411286
Chase <zedrikchase@gmail.com>
2025-12-12 11:56:15 +0800

    Comparison Operators
    Python comparison operators compare two values and return True or False.

---
+++ python_comparison_operators.py
@@ -0,0 +1,25 @@
+# Comparison operators are used to compare two values:
+x = 28
+y = 1
+
+# Equal to
+print("This is for equal to operator:")
+print(x == y)  
+
+# Not equal to
+print("This is for not equal to operator:")
+print(x != y)  
+
+# Greater than
+print("This is for greater than operator:")
+print(x > y)  
+# Less than
+print("This is for less than operator:")
+print(x < y)  
+
+# Greater than or equal to
+print("This is for greater than or equal to operator:")
+print(x >= y)
+# Less than or equal to
+print("This is for less than or equal to operator:")
+print(x <= y)
