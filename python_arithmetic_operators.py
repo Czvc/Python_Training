16cc030afe53342646160e94e89de8cb4bbb331b
Chase <zedrikchase@gmail.com>
2025-12-12 10:16:24 +0800

    Arithmetic Operators
    This section also shows common mathematical operations

---
+++ python_arithmetic_operators.py
@@ -0,0 +1,33 @@
+# Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+# Addition
+a = 10
+b = 5
+sum_result = a + b  
+print("Addition:", sum_result)
+
+# Subtraction
+sub_result = a - b  
+print("Subtraction:", sub_result)
+
+# Multiplication
+mul_result = a * b  
+print("Multiplication:", mul_result)
+
+# Division
+div_result = a / b  
+print("Division:", div_result)
+
+# Modulus
+mod_result = a % b  
+print("Modulus:", mod_result)
+
+# Exponentiation
+exp_result = a ** b  
+print("Exponentiation:", exp_result)
+
+# Floor Division
+floor_div_result = a // b  
+print("Floor Division:", floor_div_result)
+
+
