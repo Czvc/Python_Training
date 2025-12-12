3bce25ca8749d66b5469c155694c80605300033f
Chase <zedrikchase@gmail.com>
2025-12-12 12:06:47 +0800

    Update python_comparison_operators.py
    Added chained comparison example

--- python_comparison_operators.py
+++ python_comparison_operators.py
@@ -13,6 +13,7 @@ print(x != y)
 # Greater than
 print("This is for greater than operator:")
 print(x > y)  
+
 # Less than
 print("This is for less than operator:")
 print(x < y)  
@@ -23,3 +24,11 @@ print(x >= y)
 # Less than or equal to
 print("This is for less than or equal to operator:")
 print(x <= y)
+
+# Python allows you to chain comparison operators:
+a = 5
+print("This is for chained comparison operators:")
+print(1 < a < 10)  # True because 1 < 5 and 5 < 10
+print(1 < a > 10)  # False because 5 is not greater than 10
+print(1 < a <= 5)  # True because 1 < 5 and 5 <= 5
+print(1 > a < 10)  # False because 1 is not greater than 5
