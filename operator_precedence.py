3e2abf94c499802594b45be9d304b5a488afd0ec
Chase <zedrikchase@gmail.com>
2025-12-12 16:12:08 +0800

    Create operator_precedence.py

---
+++ operator_precedence.py
@@ -0,0 +1,11 @@
+# Operator precedence describes the order in which operations are performed.
+
+# Parentheses has the highest precedence, meaning that expressions inside parentheses must be evaluated first:
+print((99 + 10) - (60 + 9))
+
+# Multiplication * has higher precedence than addition +, and therefore multiplications are evaluated before additions:
+print(8 + 6 * 10)
+
+# If two operators have the same precedence, the expression is evaluated from left to right.
+print(100 + 4 - 80 + 3)
+
