8c2c8dce2ad9f601d77501798c6087ecdf0dac6a
Chase <zedrikchase@gmail.com>
2025-12-12 10:11:49 +0800

    Operators
    This section shows how to do mathematical operations using python syntax

---
+++ python_operators.py
@@ -0,0 +1,17 @@
+# Operators are used to perform operations on variables and values in Python.
+print("This section uses numbers directly", "")
+print(60+40)  # Addition operator
+print(60-40)  # Subtraction operator
+print(60*40)  # Multiplication operator
+print(60/40)  # Division operator
+print(60%40)  # Modulus operator
+
+x = 10
+y = 20
+print("")
+print("This section uses variables", "")
+print(x+y) # Addition operator with variables
+print(x-y) # Subtraction operator with variables
+print(x*y) # Multiplication operator with variables
+print(x/y) # Division operator with variables
+print(x%y) # Modulus operator with variables
